7befa5fe8c044b1440847c3e49f5035fb9570a65
Viktor Ens <v.ens86@yahoo.de>
2024-04-18 12:18:49 +0200

    add method and test for getY() and move()

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -1,8 +1,26 @@
 package org.example;
 
 public class PlayerCharacter {
+
+    static int x;
+    static int y;
+
     public static int getX() {
         int x = 0;
         return x;
+    }
+
+    public static int getY() {
+        int y = 0;
+        return y;
+    }
+
+    public static boolean move(char arg){
+        if (arg == 'w') {
+            y = 1;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -16,4 +16,34 @@ public class PlayerCharacterTest {
         int expected = 0;
         assertEquals(expected, actual);
     }
+
+    @Test
+    void getYInitiallyReturnIntZero(){
+        // Given
+        int y = 0;
+        // When
+        int actual = PlayerCharacter.getY();
+        // Then
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenMovePlayerWithArgumentW_ThenYOneIsTrue() {
+        //Given
+        char arg = 'w';
+        //When
+        boolean actual = PlayerCharacter.move(arg);
+        //Then
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
 }
+
+
+/*Write a test that ensures that when the method move in the class PlayerCharacter is called with the argument "W",
+the game character moves one position up (after the movement, Y = 1 should be true).
+
+
+Attention: for now, only write the test, do not change anything in src/main/java.
+* */
