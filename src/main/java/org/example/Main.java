9456d6a5b1ec01399d3055a3eaddf3311e31c6d7
Viktor Ens <v.ens86@yahoo.de>
2024-04-18 17:03:44 +0200

    add input method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter a key: ");
+        char input = getInput();
+        PlayerCharacter.move(input);
+        System.out.println(PlayerCharacter.x + " " + PlayerCharacter.y);
+    }
+
+    public static char getInput(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next().charAt(0);
     }
 }
