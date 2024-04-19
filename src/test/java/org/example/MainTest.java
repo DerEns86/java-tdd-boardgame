b08bf030d6ab6eee88b558b44b2d0784b1e6529a
Viktor Ens <v.ens86@yahoo.de>
2024-04-19 15:54:29 +0200

    test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -6,3 +6,10 @@ public class MainTest {
 
 
 }
+/*
+In the command line, completely without IntelliJ or any other graphical text editor, write and run a Java Hello World program.
+
+To do this, create a new directory in the shell, create a Java file with vi, compile it, and execute it.
+
+An important aspect of this task is that you research the commands you are missing on the internet.
+ */
